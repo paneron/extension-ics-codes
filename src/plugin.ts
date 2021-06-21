f77a808824dde7a5540bd44e740176099f6e44d6
Anton Strogonoff <anton@strogonoff.name>
2021-06-21 07:12:46 +0300

    chore: bump deps, switch to Emotion 11

--- src/plugin.ts
+++ src/plugin.ts
@@ -3,7 +3,7 @@ import { makeExtension } from '@riboseinc/paneron-extension-kit';
 export default makeExtension({
   mainView: () => import('./RepoView'),
   name: "ICS Codes Registry",
-  requiredHostAppVersion: "^1.0.0-beta1",
+  requiredHostAppVersion: "^1.0.0-beta2",
   datasetMigrations: {},
   datasetInitializer: () => import('./migrations/initial'),
 });
--- src/registryConfig.tsx
+++ src/registryConfig.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 
-import { css, jsx } from '@emotion/core';
+import { css, jsx } from '@emotion/react';
 import styled from '@emotion/styled';
 
 import { GenericRelatedItemView, PropertyDetailView } from '@riboseinc/paneron-registry-kit/views/util';
