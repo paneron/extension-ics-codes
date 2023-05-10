4104a625f956aabc283de0948e5f52e68478bbb5
Anton <anton@strogonoff.name>
2023-05-10 18:03:21 +0800

    feat: bump for Paneron v2

--- src/plugin.ts
+++ src/plugin.ts
@@ -3,7 +3,7 @@ import { makeExtension } from '@riboseinc/paneron-extension-kit';
 export default makeExtension({
   mainView: () => import('./RepoView'),
   name: "ICS Codes Registry",
-  requiredHostAppVersion: "^1.0.0-beta2",
+  requiredHostAppVersion: "^2.0.0",
   datasetMigrations: {},
   datasetInitializer: () => import('./migrations/initial'),
 });
