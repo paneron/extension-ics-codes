2047bca61ab8dfa9f01ed994cc13992fb9c2d9f8
Anton <anton@strogonoff.name>
2023-05-10 18:02:56 +0800

    cleanup: stop relying on electron-log

--- src/RepoView.tsx
+++ src/RepoView.tsx
@@ -1,9 +1,6 @@
 /** @jsx jsx */
 
-import log from 'electron-log';
 import { jsx } from '@emotion/react';
-
-Object.assign(console, log);
 
 import { RegistryView } from '@riboseinc/paneron-registry-kit/views';
 import { itemClassConfiguration } from './registryConfig';
